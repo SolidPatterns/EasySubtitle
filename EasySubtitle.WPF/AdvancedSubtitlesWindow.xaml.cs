19bb16a9535dc9e12aeee83ac7c1ee8fdebfb3dc
Kemal Gültekin <kemal.gultekin@outlook.com>
2015-03-11 00:04:21 +0200

    renaming, move refactoring

---
+++ EasySubtitle.WPF/AdvancedSubtitlesWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace EasySubtitle.WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class AdvancedSubtitlesWindow : Window
+    {
+        public AdvancedSubtitlesWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
