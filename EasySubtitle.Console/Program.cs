221e4da51630df2148e4f6eda9f113a953966529
Kemal Gültekin <kemal.gultekin@outlook.com>
2015-03-11 00:08:28 +0200

    Revert "Feature/add progress bar"

--- EasySubtitle.Console/Program.cs
+++ EasySubtitle.Console/Program.cs
@@ -1,38 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using System.Text;
-using System.Threading;
 using System.Threading.Tasks;
-using EasySubtitle.WPF;
 
 namespace EasySubtitle.Console
 {
     class Program
     {
-        [STAThread]
         static void Main(string[] args)
         {
-            var app = new App();
-
-            //RunProgressDialog(app);
-
-            var advancedSearchSubtitles = new AdvancedSubtitlesWindow
-            {
-                DataContext = new SearchAdvancedSubtitleViewModel(Directory.GetFiles("F:/Videos/Series/Arrow/S3", "*.mkv"))
-            };
-            app.Run(advancedSearchSubtitles);
-
-            System.Console.ReadLine();
-        }
-
-        private static void RunProgressDialog(App app)
-        {
-            var progressDialog = new ProgressDialogWindow();
-            var progressDialogViewModel = new ProgressDialogViewModel();
-            progressDialog.DataContext = progressDialogViewModel;
-            app.Run(progressDialog);
         }
     }
 }
--- EasySubtitle.ShellExtension/FindSubtitlesExtension.cs
+++ EasySubtitle.ShellExtension/FindSubtitlesExtension.cs
@@ -5,7 +5,6 @@ using System.Linq;
 using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using EasySubtitle.WPF;
 using OSDBnet;
 using SharpShell.Attributes;
 using SharpShell.SharpContextMenu;
@@ -19,7 +18,7 @@ namespace EasySubtitle.ShellExtension
         //public void Test()
         //{
         //    IAnonymousClient client = Osdb.Login("OSTestUserAgent");
-
+            
         //}
 
         protected override bool CanShowMenu()
@@ -32,52 +31,25 @@ namespace EasySubtitle.ShellExtension
             //  Create the menu strip.
             var menu = new ContextMenuStrip();
 
-            //  Create a 'easySubtitleMenu' item.
-            var easySubtitleMenu = new ToolStripMenuItem
+            //  Create a 'count lines' item.
+            var itemCountLines = new ToolStripMenuItem
             {
-                Text = "Easy Subtitle",
+                Text = "Find subtitle(s)",
             };
 
-            var findSubtitlesMenuItem = GetFindSubtitlesMenuItem();
-            var findSubtitlesAdvancedMenuItem = GetFindSubtitlesAdvancedMenuItem();
-
-            easySubtitleMenu.DropDownItems.Add(findSubtitlesMenuItem);
-            easySubtitleMenu.DropDownItems.Add(findSubtitlesAdvancedMenuItem);
+            //  When we click, we'll call the 'CountLines' function.
+            itemCountLines.Click += (sender, args) => FindSubtitles();
 
             //  Add the item to the context menu.
-            menu.Items.Add(easySubtitleMenu);
+            menu.Items.Add(itemCountLines);
 
             //  Return the menu.
+            
             return menu;
-        }
-
-        private ToolStripMenuItem GetFindSubtitlesAdvancedMenuItem()
-        {
-            var findSubtitlesAdvancedMenuItem = new ToolStripMenuItem
-            {
-                Text = "Find Subtitle(s) (Advanced)"
-            };
-            findSubtitlesAdvancedMenuItem.Click += (sender, args) => FindSubtitlesAdvanced();
-            return findSubtitlesAdvancedMenuItem;
-        }
-
-        private ToolStripMenuItem GetFindSubtitlesMenuItem()
-        {
-            var findSubtitlesMenuItem = new ToolStripMenuItem
-            {
-                Text = "Find Subtitle(s)"
-            };
-
-            //  When we click, we'll call the 'FindSubtitles' function.
-            findSubtitlesMenuItem.Click += (sender, args) => FindSubtitles();
-            return findSubtitlesMenuItem;
         }
 
         private void FindSubtitles()
         {
-            //var app = new App();
-            //app.Run(new Progress());
-
             Task.Factory.StartNew(() =>
             {
                 IList<Task> tasks = new List<Task>();
@@ -107,14 +79,7 @@ namespace EasySubtitle.ShellExtension
 
                 //  Show the ouput.
                 MessageBox.Show("Finding subtitles completed.");
-                
             });
-        }
-
-        private void FindSubtitlesAdvanced()
-        {
-            var app = new App();
-            app.Run(new AdvancedSubtitlesWindow());
         }
     }
 }
